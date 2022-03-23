6985499a09338dff085e3f2b233d131b3af3d40b
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-03-22 20:42:35 -0400

    apos fixed

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home({ portfolios }) {
         <div id="about" className={styles.profile}>
           <img src="/user.png" />
           <h1>Seounghyun Lee</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industr&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <Footer />
       </div>
